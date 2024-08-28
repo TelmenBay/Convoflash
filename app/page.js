f6c55103f7baee163e0f257d62666f694dc3cd1c
Telmen Bayarbaatar <156386842+TelmenBay@users.noreply.github.com>
2024-08-27 20:07:53 -0400

    few changes

--- app/page.js
+++ app/page.js
@@ -1,49 +1,79 @@
-'use client'
+import React from 'react'
+import getStripe from '@/utils/get-stripe'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Box, AppBar, Button, Container, Toolbar, Typography, Grid} from '@mui/material'
+import Head from 'next/head'
 
-import { useState } from 'react'
-import {
-  Container,
-  TextField,
-  Button,
-  Typography,
-  Box,
-} from '@mui/material'
 
-export default function Generate() {
-  const [text, setText] = useState('')
-  const [flashcards, setFlashcards] = useState([])
+const handleSubmit = async () => {
+  const checkoutSession = await fetch('/api/checkout_sessions', {
+    method: 'POST',
+    headers: { origin: 'http://localhost:3000' },
+  })
+  const checkoutSessionJson = await checkoutSession.json()
 
-  const handleSubmit = async () => {
-    // We'll implement the API call here
+  const stripe = await getStripe()
+  const {error} = await stripe.redirectToCheckout({
+    sessionId: checkoutSessionJson.id,
+  })
+
+  if (error) {
+    console.warn(error.message)
   }
+}
 
-  return (
-    <Container maxWidth="md">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Generate Flashcards
+export default function home() {
+
+
+  return(
+    <Container maxWidth="100vw">
+      <Head>
+        <title>Convoflash</title>
+        <meta name = "description" content="Create flashcard from your text" />
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            Convoflash
+          </Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">Login</Button>
+            <Button color="inherit" href="/sign-up">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+
+      <Box sx={{textAlign: 'center', my: 4}}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to Convoflash
         </Typography>
-        <TextField
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          label="Enter text"
-          fullWidth
-          multiline
-          rows={4}
-          variant="outlined"
-          sx={{ mb: 2 }}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-          fullWidth
-        >
-          Generate Flashcards
+        <Typography variant="h5" component="h2" gutterBottom>
+          The easiest way to create flashcards from your text.
+        </Typography>
+        <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
+          Get Started
+        </Button>
+        <Button variant="outlined" color="primary" sx={{mt: 2}}>
+          Learn More
         </Button>
       </Box>
-      
-      {/* We'll add flashcard display here */}
+      <Box sx={{my: 6}}>
+        <Typography variant="h4" component="h2" gutterBottom>Features</Typography>
+        <Grid container spacing={4}>
+          {/* Feature items */}
+        </Grid>
+      </Box>
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4" component="h2" gutterBottom>Pricing</Typography>
+        <Grid container spacing={4} justifyContent="center">
+          {}
+        </Grid>
+      </Box>
+
     </Container>
-  )
-}
+      
+  )}
