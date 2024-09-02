924fbde8095de80d2b987cd68827a634a42d9416
Telmen Bayarbaatar <156386842+TelmenBay@users.noreply.github.com>
2024-09-02 02:01:35 -0400

    logo and basics

--- app/page.js
+++ app/page.js
@@ -1,156 +1,128 @@
-'use client'
-import React from 'react'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import { Box, AppBar, Button, Container, Toolbar, Typography, Grid} from '@mui/material'
-import Head from 'next/head'
-import getStripe from '../utils/get-stripe'
+'use client';
+import React from 'react';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { Box, AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+import Head from 'next/head';
+import getStripe from '../utils/get-stripe';
+import Image from 'next/image';
 
 export default function Home() {
-
   const handleSubmit = async () => {
     const checkoutSession = await fetch('/api/checkout_session', {
       method: 'POST',
       headers: { origin: 'http://localhost:3000' },
-    })
-    const checkoutSessionJson = await checkoutSession.json()
+    });
+    const checkoutSessionJson = await checkoutSession.json();
 
     if (checkoutSession.statusCode === 500) {
-      console.error(checkoutSession.message)
-      return
+      console.error(checkoutSession.message);
+      return;
     }
 
-    const stripe = await getStripe()
-    const {error} = await stripe.redirectToCheckout({
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
       sessionId: checkoutSessionJson.id,
-    })
+    });
 
     if (error) {
-      console.warn(error.message)
+      console.warn(error.message);
     }
-  }
-  return(
-    <Container maxWidth="100vw">
-      <Head>
-        <title>Convoflash</title>
-        <meta name="description" content="Create flashcard from your text" />
-      </Head>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{flexGrow: 1}}>
-            Convoflash
-          </Typography>
-          <SignedOut>
-            <Button color="inherit" href="/sign-in">Login</Button>
-            <Button color="inherit" href="/sign-up">Sign Up</Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </Toolbar>
-      </AppBar>
+  };
 
-      <Box sx={{textAlign: 'center', my: 4}}>
-        <Typography variant="h2" component="h1" gutterBottom>
-          Welcome to Convoflash
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Have Conversations Anytime Anywhere
-        </Typography>
-        <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
-          Get Started
-        </Button>
-        <Button variant="outlined" color="primary" sx={{mt: 2}} href="https://github.com/TelmenBay/Convoflash">
-          Learn More
-        </Button>
-      </Box>
-      <Box sx={{my: 6}}>
-        <Typography variant="h4" gutterBottom sx={{ textAlign: 'center' }}>Features</Typography>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <Box sx={{
-              p:3,
-              border: '1px solid',
-              borderColor: 'grey.300',
-              borderRadius: 2,
-            }}>
-            <Typography variant='h6' gutterBottom sx={{ textAlign: 'center' }}>Easy Text Input</Typography>
-            <Typography sx={{ textAlign: 'center' }}>
-              {' '}
-              Simply input your &quot;Target Language&quot; and &quot;Source Language&quot;. Our software will do the rest.
-            </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={{
-              p:3,
-              border: '1px solid',
-              borderColor: 'grey.300',
-              borderRadius: 2,
-            }}>
-            <Typography variant='h6' gutterBottom sx={{ textAlign: 'center' }}>Smart Flashcards</Typography>
-            <Typography sx={{ textAlign: 'center' }}>
-              {' '}
-              Our AI Intelligently breaks down your text into concise flashcards, perfect for studying.
-            </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Box sx={{
-              p:3,
-              border: '1px solid',
-              borderColor: 'grey.300',
-              borderRadius: 2,
-            }}>
-            <Typography variant='h6' gutterBottom sx={{ textAlign: 'center' }}>No Language Barrier</Typography>
-            <Typography sx={{ textAlign: 'center' }}>
-              {' '}
-              Accessible to anyone around the world from any device. Talk to anyone with Convoflash.
-            </Typography>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-      <Box sx={{my: 6, textAlign: 'center'}}>
-        <Typography variant="h4" gutterBottom>Pricing</Typography>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
-            <Box sx={{
-              p:3,
-              border: '1px solid',
-              borderColor: 'grey.300',
-              borderRadius: 2,
-            }}>
-              <Typography variant='h5' gutterBottom>Free</Typography>
-              <Typography variant='h6' gutterBottom>No Payment</Typography>
-              <Typography>
-                {' '}
-                Access to basic flashcard features and limited storage.
-              </Typography>
-              <Button variant='contained' color='primary' sx={{mt: 2}} href='/generate'>
-                Choose Free
-              </Button>
-            </Box> 
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box sx={{
-              p:3,
-              border: '1px solid',
-              borderColor: 'grey.300',
-              borderRadius: 2,
-            }}>
-              <Typography variant='h5' gutterBottom>Pro</Typography>
-              <Typography variant='h6' gutterBottom>$1.99 / One Time Payment</Typography>
-              <Typography>
-                {' '}
-                Unlimited flashcards and storage, with priority support.
-              </Typography>
-              <Button variant='contained' color='primary' sx={{mt: 2}} onClick={handleSubmit}>
-                Choose Pro
-              </Button>
-            </Box> 
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
-  )
+  return React.createElement(
+    Container,
+    { maxWidth: '100vw' },
+    React.createElement(
+      Head,
+      null,
+      React.createElement('title', null, 'Convoflash'),
+      React.createElement('meta', {
+        name: 'description',
+        content: 'Create flashcard from your text',
+      })
+    ),
+    React.createElement(
+      AppBar,
+      { position: 'static' },
+      React.createElement(
+        Toolbar,
+        null,
+        React.createElement(Image, {
+          src: '/brand.png',
+          alt: 'Convoflash Logo',
+          width: 160,
+          height: 40,
+        }),
+        React.createElement('div', { style: { flexGrow: 1 } }), // This will push the elements to the right
+        React.createElement(
+          SignedOut,
+          null,
+          React.createElement(
+            Button,
+            {
+              variant: 'outlined',
+              color: 'inherit',
+              href: 'https://github.com/TelmenBay/Convoflash',
+              sx: { mr: 2 },
+            },
+            'Learn More'
+          ),
+          React.createElement(
+            Button,
+            {
+              variant: 'outlined',
+              color: 'inherit',
+              href: '/sign-in',
+              sx: { mr: 2 },
+            },
+            'Login'
+          ),
+          React.createElement(
+            Button,
+            { variant: 'outlined', color: 'inherit', href: '/sign-up' },
+            'Sign Up'
+          )
+        ),
+        React.createElement(
+          SignedIn,
+          null,
+          React.createElement(
+            Button,
+            {
+              variant: 'outlined',
+              color: 'primary',
+              sx: { mt: 2 },
+              href: 'https://github.com/TelmenBay/Convoflash',
+            },
+            'Learn More'
+          ),
+          React.createElement(UserButton, null)
+        )
+      )
+    ),
+    React.createElement(
+      Box,
+      { sx: { textAlign: 'center', my: 4 } },
+      React.createElement(
+        Typography,
+        { variant: 'h2', component: 'h1', gutterBottom: true },
+        'Welcome to Convoflash'
+      ),
+      React.createElement(
+        Typography,
+        { variant: 'h5', component: 'h2', gutterBottom: true },
+        'Have Conversations Anytime Anywhere'
+      ),
+      React.createElement(
+        Button,
+        {
+          variant: 'contained',
+          color: 'primary',
+          sx: { mt: 2, mr: 2 },
+          href: '/generate',
+        },
+        'Get Started'
+      )
+    )
+  );
 }
